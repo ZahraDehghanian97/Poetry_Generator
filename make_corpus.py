16c299ca6126b85642c229fe71c6d3a87db4a320
Zahra Dehghanian <z.dehghanian@aut.ac.ir>
2021-07-20 20:00:37 +0430

    change corpus generation

--- make_corpus.py
+++ make_corpus.py
@@ -1,11 +1,29 @@
 import os
-corpus_address = "./shahname"
-corpus = ""
-for filename in os.listdir(corpus_address):
-   with open( corpus_address+"/"+filename, encoding='utf-8') as f:
-      for line in f :
-         corpus += line
+import random
+import pickle
 
-file_corpus = open("shahname.txt", "a")
-file_corpus.write(corpus)
-file_corpus.close()
+corpus_address = "./shahname"
+data_path = "my_shahname_represntation.txt"
+
+corpus = ""
+test = []
+list_dir = random.sample(os.listdir(corpus_address), len(os.listdir(corpus_address)))
+num_text = len(list_dir)
+num_test = int(0.1 * num_text)
+counter = 0
+for filename in list_dir:
+    with open(corpus_address + "/" + filename, encoding='utf-8') as f:
+        if counter < num_test:
+            temp = ""
+            for line in f:
+                temp += line
+            test.append(temp)
+        else:
+            for line in f:
+                corpus += line
+    counter += 1
+with open(data_path, 'wb') as f:
+    pickle.dump((corpus, test), f)
+print(num_text)
+print(len(test))
+print(test[0])
